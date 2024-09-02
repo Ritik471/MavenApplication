2c4ca04abab35c9675764387deee9e83ee40695b
Ritik Shah <ritikshah1206@gmail.com>
2024-09-02 10:18:21 +0530

    first commit

---
+++ src/main/java/com/7TC5/app/App.java
@@ -0,0 +1,13 @@
+package com.7TC5.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
